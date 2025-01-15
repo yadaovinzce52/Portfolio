6fd593ab4d71beb38306da12e29049a238de8832
Vinzce Yadao <yadaovinzce@gmail.com>
2025-01-15 14:43:51 -0500

    changed app route to home

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/index.html')
+@app.route('/')
 def hello_world():  # put application's code here
     return render_template('index.html')
 

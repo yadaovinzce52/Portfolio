0d608ccfdd78d6883d09e8c65acc17c2bdd6672e
Vinzce Yadao <yadaovinzce@gmail.com>
2025-10-15 19:49:29 -0400

    Finish About Page

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap5
+from datetime import datetime
 
 app = Flask(__name__)
-Bootstrap(app)
+Bootstrap5(app)
+curr_date = datetime.now().date()
+curr_year = datetime.now().year
 
 
 @app.route('/')
 def home():  # put the application's code here
-    return render_template('home.html')
+    skills_front = ['Bootstrap', 'JavaScript', 'HTML', 'CSS', 'jQuery']
+    skills_back = ['Python', 'Java', 'PL/SQL', 'Microservices', 'Oracle Cloud Infrastructure', 'Oracle']
+    skills_tools = ['Git', 'JetBrains IDEs', 'Microsoft Azure', 'Postman', 'Atlassian Products']
+    skills_other = ['RESTful APIs', 'CI/CD Pipelines', 'Test-Driven Development', 'Cross-Device compatibility',
+                    'Agile Development', 'Object-Oriented Programming', 'Data Structures', 'Algorithms', 'JSON']
+    skills = {'Frontend': skills_front, 'Backend': skills_back, 'Tools': skills_tools, 'Other': skills_other}
+    return render_template('home.html', skills=skills, curr_year=curr_year)
 
 
 @app.route('/projects')
@@ -19,11 +28,6 @@ def projects():
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-
-@app.route('/hobbies')
-def hobbies():
-    return render_template('hobbies.html')
 
 
 @app.route('/contact')
